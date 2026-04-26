595cc4b60eda48e2c48010056ee19c0243762331
YOUR_NAME <YOUR_EMAIL>
2026-04-26 08:59:02 -0400

    okokokokokoko

--- randompage.js
+++ randompage.js
@@ -1,11 +1,11 @@
 function goToRandomPage() {
     const links = [
-        "Biomes.html",
-        "Bosses.html",
-        "Events.html",
-        "Lab.html",
-        "NPCs.html",
-        "QandA.html"
+        "https://garrett3705.github.io/cis3020lab/index.html",
+        "https://garrett3705.github.io/cis3020lab/NPCs.html",
+        "https://garrett3705.github.io/cis3020lab/Events.html",
+        "https://garrett3705.github.io/cis3020lab/Bosses.html",
+        "https://garrett3705.github.io/cis3020lab/Biomes.html",
+        "https://garrett3705.github.io/cis3020lab/QandA.html"
     ];
 
     const randomIndex = Math.floor(Math.random() * links.length);
